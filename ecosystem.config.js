64c26c1fdcfca497bb73388691460d609769f4e5
SierikovRoman <rebit1love@gmail.com>
2019-07-10 07:59:24 +0300

    fix 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-3-19-57-96.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/id_rsa.pub',
+        key: '~/.ssh/id_rsa',
         ref: 'origin/master',
         repo: 'git@github.com:SierikovRoman/chat-server.git',
         path: '/home/ubuntu/chat-server',
